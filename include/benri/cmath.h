d750dbae7c914d9d6d7fa727b3df5c738aa3693c
Jan Sende <jansende@users.noreply.github.com>
2018-11-01 14:03:41 +0100

    the remove_cvref apperently is coming in c++20

--- include/benri/cmath.h
+++ include/benri/cmath.h
@@ -8,6 +8,16 @@
 #include <cstdlib>
 #include <cmath>
 #include <algorithm>
+namespace std
+{
+template <class T>
+struct remove_cvref
+{
+    typedef std::remove_cv_t<std::remove_reference_t<T>> type;
+};
+template <class T>
+using remove_cvref_t = typename remove_cvref<T>::type;
+} // namespace std
 
 namespace benri
 {
