b114d8a22dc6ef6d0c2ac70960b16db4b1654cd0
Jan Sende <jansende@users.noreply.github.com>
2018-12-06 16:34:38 +0100

    fixed a msvc bug

--- include/benri/impl/config.h
+++ include/benri/impl/config.h
@@ -7,4 +7,14 @@ using Precision = double;
 #else
 using Precision = BENRI_PRECISION;
 #endif
+//The following is a fix for msvc: clang and gcc handle
+//[[nodiscard]] just fine, even when not in c++17 mode,
+//but msvc will not compile.
+#ifdef _WIN32
+#if __cplusplus >= 201703L
+//everything alright, nothing to do
+#else
+//remove the nodiscard attribute
+#define nodiscard
+#endif
 } // namespace benri
