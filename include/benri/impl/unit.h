c8f25503269c4783dea54e9b264f24bd41668e9c
Jan Sende <jansende@users.noreply.github.com>
2019-05-07 22:24:52 +0200

    made some minor performance updates
    - introduced the detector idiom, which reduces code bloat
    - replaced the sorting function with a insertion_sort algorithm
    - removed some unused functions

--- include/benri/impl/unit.h
+++ include/benri/impl/unit.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <benri/impl/list.h>
 #include <benri/impl/sorting.h>
+#include <benri/impl/detector.h>
 
 namespace benri
 {
@@ -12,137 +13,51 @@ struct unit
     using dimensions = Dimensions;
     using prefix = Prefix;
 };
-//The has_type function checks if a type has the ::dimensions attribute
-//using SFINAE.
-template <class T>
-struct has_dimensions
-{
-    template <class U>
-    static constexpr auto test(typename U::dimensions *) { return true; }
-    template <class U>
-    static constexpr auto test(...) { return false; }
-
-    static constexpr auto value = test<T>(nullptr);
-};
-template <class T>
-constexpr bool has_dimensions_v = has_dimensions<T>::value;
-//The dimension_is_list function checks if a given type has a ::dimensions
-//attribute which is a list. The check is only done, when the given
-//bool is true.
-template <bool, class T>
-struct dimensions_is_list_impl;
-template <class T>
-struct dimensions_is_list_impl<true, T>
-{
-    static constexpr auto value = impl::is_list_v<typename T::dimensions>;
-};
-template <class T>
-struct dimensions_is_list_impl<false, T>
-{
-    static constexpr auto value = false;
-};
-template <bool Check, class T>
-struct dimensions_is_list : std::integral_constant<bool, dimensions_is_list_impl<Check, T>::value>
-{
-};
-template <bool Check, class T>
-constexpr bool dimensions_is_list_v = dimensions_is_list<Check, T>::value;
 //The has_valid_dimensions function checks if a given type has a ::dimensions
 //attribute which is a list.
 template <class T>
-struct has_valid_dimensions : std::integral_constant<bool, dimensions_is_list_v<has_dimensions_v<T>, T>>
-{
-};
-template <class T>
-constexpr bool has_valid_dimensions_v = has_valid_dimensions<T>::value;
-//The has_type function checks if a type has the ::prefix attribute
-//using SFINAE.
-template <class T>
-struct has_prefix
-{
-    template <class U>
-    static constexpr auto test(typename U::prefix *) { return true; }
-    template <class U>
-    static constexpr auto test(...) { return false; }
-
-    static constexpr auto value = test<T>(nullptr);
-};
-template <class T>
-constexpr bool has_prefix_v = has_prefix<T>::value;
-//The prefix_is_list function checks if a given type has a ::prefix
-//attribute which is a list. The check is only done, when the given
-//bool is true.
-template <bool, class T>
-struct prefix_is_list_impl;
-template <class T>
-struct prefix_is_list_impl<true, T>
-{
-    static constexpr auto value = impl::is_list_v<typename T::prefix>;
-};
-template <class T>
-struct prefix_is_list_impl<false, T>
-{
-    static constexpr auto value = false;
-};
-template <bool Check, class T>
-struct prefix_is_list : std::integral_constant<bool, prefix_is_list_impl<Check, T>::value>
-{
-};
-template <bool Check, class T>
-constexpr bool prefix_is_list_v = prefix_is_list<Check, T>::value;
+using has_valid_dimensions = typename std::enable_if<detect_if<typename T::dimensions, impl::is_list>>::type;
 //The has_valid_prefix function checks if a given type has a ::prefix
 //attribute which is a list.
 template <class T>
-struct has_valid_prefix : std::integral_constant<bool, prefix_is_list_v<has_prefix_v<T>, T>>
-{
-};
-template <class T>
-constexpr bool has_valid_prefix_v = has_valid_prefix<T>::value;
+using has_valid_prefix = typename std::enable_if<detect_if<typename T::prefix, impl::is_list>>::type;
 //The is_unit function checks if a type has a ::dimensions and a ::prefix
 //attribute. This does not mean, we have an unit type, but we do not
 //care, as we can do the necessary calculations.
 template <class T>
-struct is_unit : std::integral_constant<bool, has_valid_dimensions_v<T> && has_valid_prefix_v<T>> //TODO: - check if Power is a ratio
-{
-};
-template <class T>
-constexpr bool is_unit_v = is_unit<T>::value;
+using is_unit = typename std::enable_if<detect_if<T, has_valid_dimensions> && detect_if<T, has_valid_prefix>>::type;
 //TODO: - Put this into a unit test folder.
 //basic tests
-static_assert(!is_unit_v<int>, "");
-static_assert(!is_unit_v<unit<double, double>>, "");
-static_assert(is_unit_v<unit<list<>, list<>>>, "");
+static_assert(!detect_if<int, is_unit>, "");
+static_assert(!detect_if<unit<double, double>, is_unit>, "");
+static_assert(detect_if<unit<list<>, list<>>, is_unit>, "");
+//The is_dimensionless function checks if a given unit is dimensionless.
+//(The dimension is an empty list.)
 template <class T>
-struct is_dimensionless : std::integral_constant<bool, std::is_same<typename T::dimensions, list<>>::value>
-{
-};
+using is_dimensionless = typename std::enable_if<detect_if<T, is_unit> && std::is_same<typename T::dimensions, list<>>::value>::type;
+//The is_one function checks if a given unit is a factor of one.
+//(The dimension and the prefix are an empty list.)
 template <class T>
-constexpr bool is_dimensionless_v = is_dimensionless<T>::value;
-template <class T>
-struct is_one : std::integral_constant<bool, std::is_same<typename T::dimensions, list<>>::value && std::is_same<typename T::prefix, list<>>::value>
-{
-};
-template <class T>
-constexpr bool is_one_v = is_one<T>::value;
+using is_one = typename std::enable_if<detect_if<T, is_unit> && std::is_same<typename T::dimensions, list<>>::value && std::is_same<typename T::prefix, list<>>::value>::type;
 #pragma endregion
 #pragma region unit functions
 #pragma region multiplication
 //The multiply_units function multiplies two units.
-template <class...Lists>
-struct multiply_units_impl {
-    using type = void;
+template <class Dimensions, class Prefixes, class... Units>
+struct multiply_units_impl
+{
 };
-template <class lhsUnit, class rhsUnit>
-struct multiply_units_impl<lhsUnit, rhsUnit> {
-    using type = unit<multiply_lists<typename lhsUnit::dimensions, typename rhsUnit::dimensions>, multiply_lists<typename lhsUnit::prefix, typename rhsUnit::prefix>>;
+template <class...Dimensions, class...Prefixes>
+struct multiply_units_impl<list<Dimensions...>,list<Prefixes...>>
+{
+    using type = unit<multiply_lists<Dimensions...>,multiply_lists<Prefixes...>>;
 };
-template <class lhsUnit, class rhsUnit, class...Rest>
-struct multiply_units_impl<lhsUnit, rhsUnit, Rest...> {
-    using type = typename multiply_units_impl<typename multiply_units_impl<lhsUnit, rhsUnit>::type, Rest...>::type;
+template <class...Dimensions, class...Prefixes, class Unit, class... Rest>
+struct multiply_units_impl<list<Dimensions...>,list<Prefixes...>,Unit, Rest...> : multiply_units_impl<list<Dimensions...,typename Unit::dimensions>,list<Prefixes...,typename Unit::prefix>,Rest...>
+{
 };
-
-template <class...Lists>
-using multiply_units = typename multiply_units_impl<Lists...>::type;
+template <class... Lists>
+using multiply_units = typename multiply_units_impl<list<>,list<>,Lists...>::type;
 #pragma endregion
 #pragma region power
 //The pow_unit function applies a given power to a unit.
@@ -152,22 +67,18 @@ using pow_unit = unit<pow_list<typename Unit::dimensions, Power>, pow_list<typen
 #pragma region division
 //The divide_units function divide two units.
 template <class lhsUnit, class rhsUnit>
-using divide_units = unit<divide_lists<typename lhsUnit::dimensions, typename rhsUnit::dimensions>,divide_lists<typename lhsUnit::prefix, typename rhsUnit::prefix>>;
+using divide_units = unit<divide_lists<typename lhsUnit::dimensions, typename rhsUnit::dimensions>, divide_lists<typename lhsUnit::prefix, typename rhsUnit::prefix>>;
 #pragma endregion
 #pragma endregion
 #pragma region unit compatibility checker
 //The is_compatible function checks if two units should be handled
 //as if they are equivalent, even if they are not.
 template <class LDimensions, class LPrefix, class RDimensions, class RPrefix>
-struct is_compatible_impl : std::false_type
+struct is_compatible_with_impl : std::false_type
 {
 };
 template <class L, class R>
-struct is_compatible : std::integral_constant<bool, is_compatible_impl<typename L::dimensions, typename L::prefix, typename R::dimensions, typename R::prefix>::value>
-{
-};
-template <class L, class R>
-constexpr bool is_compatible_v = is_compatible<L, R>::value;
+using is_compatible_with = typename std::enable_if<is_compatible_with_impl<typename L::dimensions, typename L::prefix, typename R::dimensions, typename R::prefix>::value || is_compatible_with_impl<typename R::dimensions, typename R::prefix, typename L::dimensions, typename L::prefix>::value>::type;
 #pragma endregion
 #pragma region remove prefix
 template <class Unit>
