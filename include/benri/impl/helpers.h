86cefd685d1a89f95aa62d0f09f9b11fb41fd1f9
Jan Sende <jansende@users.noreply.github.com>
2019-11-01 23:59:22 +0100

    Added the make_quantity, and make_quantity_point template function
    - It allows making quantities, and quantity points from a unit and a
      value, automatically static casting to Precision.

--- include/benri/impl/helpers.h
+++ include/benri/impl/helpers.h
@@ -9,4 +9,14 @@ template <class... Dims>
 using make_dimension = type::sort<type::list<Dims...>>;
 template <class Dimension, class Prefix = type::list<>>
 using make_unit = unit<type::sort<Dimension>, type::sort<Prefix>>;
+template <class Unit, class ValueType = Precision, class T = Precision>
+constexpr auto make_quantity(const T& value) -> quantity<Unit, ValueType>
+{
+    return quantity<Unit, ValueType>{static_cast<ValueType>(value)};
+}
+template <class Unit, class ValueType = Precision, class T = Precision>
+constexpr auto make_quantity_point(const T& value) -> quantity_point<Unit, ValueType>
+{
+    return quantity_point<Unit, ValueType>{static_cast<ValueType>(value)};
+}
 } // namespace benri
--- include/benri/si/macros.h
+++ include/benri/si/macros.h
@@ -7,69 +7,65 @@
     using NAME##_t = benri::make_unit<DIMENSION, PREFIX>;                              \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(long double value) noexcept \
     {                                                                                  \
-        return benri::quantity<NAME##_t, benri::Precision>{                            \
-            static_cast<benri::Precision>(value)};                                     \
+        return benri::make_quantity<NAME##_t, benri::Precision>(value);                \
     }                                                                                  \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(                            \
         unsigned long long int value) noexcept                                         \
     {                                                                                  \
-        return benri::quantity<NAME##_t, benri::Precision>{                            \
-            static_cast<benri::Precision>(value)};                                     \
+        return benri::make_quantity<NAME##_t, benri::Precision>(value);                \
     }                                                                                  \
-    constexpr auto NAME =                                                              \
-        benri::quantity<NAME##_t, benri::Precision>{static_cast<benri::Precision>(1)};
+    constexpr auto NAME = benri::make_quantity<NAME##_t, benri::Precision>(1);
 #define implement_unit_point(NAME, DIMENSION, PREFIX)                                  \
     using NAME##_t = benri::make_unit<DIMENSION, PREFIX>;                              \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(long double value) noexcept \
     {                                                                                  \
-        return benri::quantity_point<NAME##_t, benri::Precision>{                      \
-            static_cast<benri::Precision>(value)};                                     \
+        return benri::make_quantity_point<NAME##_t, benri::Precision>(value);          \
     }                                                                                  \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(                            \
         unsigned long long int value) noexcept                                         \
     {                                                                                  \
-        return benri::quantity_point<NAME##_t, benri::Precision>{                      \
-            static_cast<benri::Precision>(value)};                                     \
+        return benri::make_quantity_point<NAME##_t, benri::Precision>(value);          \
     }                                                                                  \
-    constexpr auto NAME = benri::quantity_point<NAME##_t, benri::Precision>{           \
-        static_cast<benri::Precision>(1)};
+    constexpr auto NAME = benri::make_quantity_point<NAME##_t, benri::Precision>(1);
 
 #define link_unit(NAME, ALIAS)                                                         \
     using NAME##_t = ALIAS##_t;                                                        \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(long double value) noexcept \
     {                                                                                  \
-        return benri::quantity<NAME##_t>{static_cast<benri::Precision>(value)};        \
+        return benri::make_quantity<NAME##_t, benri::Precision>(value);                \
     }                                                                                  \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(                            \
         unsigned long long int value) noexcept                                         \
     {                                                                                  \
-        return benri::quantity<NAME##_t>{static_cast<benri::Precision>(value)};        \
+        return benri::make_quantity<NAME##_t, benri::Precision>(value);                \
     }                                                                                  \
     constexpr auto NAME = ALIAS;
 #define link_unit_point(NAME, ALIAS)                                                   \
     using NAME##_t = ALIAS##_t;                                                        \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(long double value) noexcept \
     {                                                                                  \
-        return benri::quantity_point<NAME##_t>{static_cast<benri::Precision>(value)};  \
+        return benri::make_quantity_point<NAME##_t, benri::Precision>(value);          \
     }                                                                                  \
     [[nodiscard]] constexpr inline auto operator"" _##NAME(                            \
         unsigned long long int value) noexcept                                         \
     {                                                                                  \
-        return benri::quantity_point<NAME##_t>{static_cast<benri::Precision>(value)};  \
+        return benri::make_quantity_point<NAME##_t, benri::Precision>(value);          \
     }
 
-#define create_symbol(NAME, DIMENSION, PREFIX)                                  \
-    constexpr auto NAME = benri::quantity<benri::make_unit<DIMENSION, PREFIX>>{ \
-        static_cast<benri::Precision>(1)};
-#define create_constant(NAME, VALUE, UNIT)                         \
-    constexpr auto NAME = benri::quantity<UNIT, benri::Precision>{ \
-        benri::expand_prefix_list<benri::Precision, VALUE>};
-#define create_symbol_point(NAME, DIMENSION, PREFIX, VALUE)                           \
-    constexpr auto NAME = benri::quantity_point<benri::make_unit<DIMENSION, PREFIX>>{ \
-        benri::expand_prefix_list<benri::Precision, VALUE>};
-#define create_constant_point(NAME, VALUE, UNIT)                         \
-    constexpr auto NAME = benri::quantity_point<UNIT, benri::Precision>{ \
-        benri::expand_prefix_list<benri::Precision, VALUE>};
+#define create_symbol(NAME, DIMENSION, PREFIX) \
+    constexpr auto NAME =                      \
+        benri::make_quantity<benri::make_unit<DIMENSION, PREFIX>, benri::Precision>(1);
+#define create_constant(NAME, VALUE, UNIT)                              \
+    constexpr auto NAME = benri::make_quantity<UNIT, benri::Precision>( \
+        benri::expand_prefix_list<benri::Precision, VALUE>);
+#define create_symbol_point(NAME, DIMENSION, PREFIX, VALUE)             \
+    constexpr auto NAME =                                               \
+        benri::make_quantity_point<benri::make_unit<DIMENSION, PREFIX>, \
+                                   benri::Precision>(                   \
+            benri::expand_prefix_list<benri::Precision, VALUE>);
+#define create_constant_point(NAME, VALUE, UNIT)                              \
+    constexpr auto NAME = benri::make_quantity_point<UNIT, benri::Precision>( \
+        benri::expand_prefix_list<benri::Precision, VALUE>);
 
 #define create_and_register_dimension(NAME, ...) \
     using NAME##_t = benri::make_dimension<__VA_ARGS__>;
